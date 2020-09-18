e684c3a62ea4ee6ed81ea49e4accabe896c55a9c
Project Survey 99 <ecochirp.hotmail@gmail.com>
2020-09-18 10:19:58 +0700

    Initial Commit

---
+++ BluPrint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluPrint
+//
+//  Created by Nobel Suhendra on 18/09/20.
+//  Copyright © 2020 BluPrint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
